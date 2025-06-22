173388a37a7c3892d51d6e9694aeae38ea4814bc
Rene-Yanez <rene.yanez2001@alumnos.ubiobio.cl>
2025-06-22 18:52:34 -0400

    informes

---
+++ src/main/java/com/farmaciascomunales/informes/InformesApplication.java
@@ -0,0 +1,13 @@
+package com.farmaciascomunales.informes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformesApplication.class, args);
+	}
+
+}
